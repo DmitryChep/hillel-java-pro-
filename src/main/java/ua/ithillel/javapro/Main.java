c2e8ba255cedff78d90a30eb4324b06ac1345616
Dmitry <dmitrych5129@gmail.com>
2024-09-29 19:06:08 +0300

    Created mass unit converter (from kg to pounds)

---
+++ src/main/java/ua/ithillel/javapro/Main.java
@@ -0,0 +1,17 @@
+package ua.ithillel.javapro;
+
+public class Main {
+    private static final double CONV_K = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Converter App.");
+
+        double kgs = 10;
+        double pounds = convKgsToPounds(kgs);
+        System.out.println("Result is " + pounds + " pounds.");
+    }
+
+    private static double convKgsToPounds(double kgs){
+        return  kgs * CONV_K;
+    }
+}
