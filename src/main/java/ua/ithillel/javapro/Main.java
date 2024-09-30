60fbc027a9caa97737836d90861f35fff8995543
Dmitry <dmitrych5129@gmail.com>
2024-09-30 22:18:00 +0300

    Added conversion from kilometers to miles

--- src/main/java/ua/ithillel/javapro/Main.java
+++ src/main/java/ua/ithillel/javapro/Main.java
@@ -6,12 +6,26 @@ public class Main {
         ScannerUserInput scannerUserInput = new ScannerUserInput();
 
         Printer.printTitle();
-        Printer.printRequestForUser();
-
-        scannerUserInput.setUserDoubleInput(scannerUserInput.getScannerDouble().nextInt());
-        converter.setMiles(scannerUserInput.getUserDoubleInput());
-        double resultConversion = converter.convertMilesToKilometers(converter.getMiles());
-        Printer.printResultsConversion( resultConversion);
+        Printer.printRequestForUser(1);
+        scannerUserInput.setUserStringInput(scannerUserInput.getScannerString().nextLine());
+        //
+        if (scannerUserInput.getUserStringInput().trim().equalsIgnoreCase("mile")) {
+            Printer.printRequestForUser(2);
+            scannerUserInput.setUserDoubleInput(scannerUserInput.getScannerDouble().nextInt());
+            converter.setMiles(scannerUserInput.getUserDoubleInput());
+            double resultConversion = converter.convertMilesToKilometers(converter.getMiles());
+            Printer.printResultsConversion("resultKilometers", resultConversion);
+            //
+        } else if (scannerUserInput.getUserStringInput().trim().equalsIgnoreCase("kilometer")) {
+            Printer.printRequestForUser(3);
+            scannerUserInput.setUserDoubleInput(scannerUserInput.getScannerDouble().nextInt());
+            converter.setKilometers(scannerUserInput.getUserDoubleInput());
+            double resultConversion = converter.convertKilometersToMiles(converter.getKilometers());
+            Printer.printResultsConversion("resultMiles", resultConversion);
+        } else {
+            System.out.println("Error: Incorrect metric unit entry");
+        }
     }
 }
+
 
