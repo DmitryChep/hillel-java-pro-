53f3495728bd8b690419e3d805b2cfafe7aa74be
Dmitry <dmitrych5129@gmail.com>
2024-09-30 22:10:52 +0300

    Created converter from miles to kilometers

--- src/main/java/ua/ithillel/javapro/Main.java
+++ src/main/java/ua/ithillel/javapro/Main.java
@@ -1,25 +1,17 @@
 package ua.ithillel.javapro;
 
 public class Main {
-    private static final double CONV_K = 2.20462;
-
     public static void main(String[] args) {
-        System.out.println("Converter App.");
-        System.out.println("Version 1.0.");
+        Converter converter = new Converter();
+        ScannerUserInput scannerUserInput = new ScannerUserInput();
 
-        double kgs = 5;
-        double pnds  = 15;
-        double pounds = convKgsToPounds(kgs);
-        double kilos  = convPoundsToKgs(pnds);
-        System.out.println("Result is " + pounds + " pounds and " + kilos + "kgs.");
+        Printer.printTitle();
+        Printer.printRequestForUser();
+
+        scannerUserInput.setUserDoubleInput(scannerUserInput.getScannerDouble().nextInt());
+        converter.setMiles(scannerUserInput.getUserDoubleInput());
+        double resultConversion = converter.convertMilesToKilometers(converter.getMiles());
+        Printer.printResultsConversion( resultConversion);
     }
-
-    private static double convKgsToPounds(double kgs){
-        return  kgs * CONV_K;
-    }
-
-    private static double convPoundsToKgs(double pnds){
-        return  pnds / CONV_K;
-    }
-
 }
+
