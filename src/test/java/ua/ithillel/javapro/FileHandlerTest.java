cbb0d130936b9c35091f407c3d8c8d1efeeafc64
Dmitry <dmitrych5129@gmail.com>
2024-10-27 17:59:26 +0200

    add unit tests for methods: writeFile, readFIle in FileHandler class.

---
+++ src/test/java/ua/ithillel/javapro/FileHandlerTest.java
@@ -0,0 +1,42 @@
+package ua.ithillel.javapro;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FileHandlerTest {
+    private FileHandler fileHandler;
+    private final String TEST_FILE_PATH = "files/testfile.txt";
+    private final String TEST_FILE_CONTENT = "This is a test text.";
+
+
+    @BeforeEach
+    void setUp() {
+        fileHandler = new FileHandler();
+        new File(TEST_FILE_PATH);
+    }
+
+    @Test
+    void writeFile_shouldWriteFileContent_whenPathAndFileContentIsRight() throws IOException {
+        String result = fileHandler.writeFile(TEST_FILE_PATH, TEST_FILE_CONTENT);
+        String fileContent = Files.readString(Path.of(TEST_FILE_PATH));
+
+        assertEquals(TEST_FILE_CONTENT, fileContent);
+        assertEquals("Success.", result);
+    }
+
+    @Test
+    void readFile_shouldReadFile_whenPathIsRight() throws IOException {
+        Files.writeString(Path.of(TEST_FILE_PATH), TEST_FILE_CONTENT);
+        String result = fileHandler.readFile(TEST_FILE_PATH);
+
+        assertEquals(TEST_FILE_CONTENT, result);
+    }
+}
