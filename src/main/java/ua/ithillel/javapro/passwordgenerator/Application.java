b3478200966b8e87d525c647001b8777e0cb31e2
Dmytro Chepurko <dmitrych5129@gmail.com>
2024-12-15 21:15:01 +0200

    add maven dependency(password generator)

--- src/main/java/ua/ithillel/javapro/passwordgenerator/Application.java
+++ src/main/java/ua/ithillel/javapro/passwordgenerator/Application.java
@@ -4,5 +4,7 @@ import ua.ithillel.javapro.PasswordGenerator;
 
 public class Application {
     public static void main(String[] args) {
+        String resultPasswordGenerator = PasswordGenerator.generatePassword(10);
+        System.out.println("The generated password in the new project is: " + resultPasswordGenerator);
     }
 }
